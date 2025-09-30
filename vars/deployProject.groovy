86f356516c293608602eff8888fbe7ea24548de3
Bharath Savadatti <bharath.savadatti447@gmail.com>
2025-09-30 22:53:07 +0530

    Update deployProject.groovy

--- vars/deployProject.groovy
+++ vars/deployProject.groovy
@@ -1,4 +1,4 @@
-def call (String deploy_Path = '', String artifact = '') {
+def call (String deploy_Path = './', String artifact = './') {
     echo "Deploying Artifact ${artifact} to ${deploy_Path}"
-    sh 'scp ${artifact} ${deploy_Path}'
+    sh 'cp ${artifact} ${deploy_Path}'
 }
