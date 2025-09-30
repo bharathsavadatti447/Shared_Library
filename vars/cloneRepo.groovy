98918f58b202b64ebfc50632441e67b5933bf766
Bharath Savadatti <bharath.savadatti447@gmail.com>
2025-09-30 22:50:59 +0530

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,4 +1,4 @@
-def call (String repo_url='', String branch = 'main') {
+def call (String repo_url='https://github.com/bharathsavadatti447/Shared_Library.git', String branch = 'main') {
     echo "Cloning the Repository ${repourl}, branch ${branch}"
     git branch: branch, url: repo_url
 }
